b76c39ea56f4e4d3d31647e8628581cbc68f4db2
Delsuin <fnhopyt@gmail.com>
2026-08-04 15:08:47 -0400

    Feature: reworked bullet image

--- pyfiles/shot.py
+++ pyfiles/shot.py
@@ -1,6 +1,7 @@
 import pygame
 from pyfiles.circleshape import CircleShape
 from pyfiles.constants import SHOT_RADIUS, LINE_WIDTH, BOMB_RADIUS
+from pyfiles.images import teleport_sprite1, teleport_rect1
 
 
 class Shot(CircleShape):
@@ -10,8 +11,11 @@ class Shot(CircleShape):
         
         
     def draw(self, screen):
+        # frame 7 for regular bullet
+        # frame 10-11 for bomb
         # change white for the shot animation using the teleport animation
-        pygame.draw.circle(screen, "white", self.position, self.radius, LINE_WIDTH)
+        screen.blit(teleport_sprite1[6], teleport_sprite1[10].get_rect(center = (self.position)))
+        pygame.draw.circle(screen, "#3C8263", self.position, self.radius, LINE_WIDTH)
         
         
     def update(self, dt):
@@ -21,10 +25,15 @@ class Bomb(Shot):
     def __init__(self, x, y):
         super().__init__(x,y, BOMB_RADIUS) 
         self.bomb = True
-
+    def draw(self, screen):
+        # frame 6 for regular bullet
+        # frame 10 for bomb
+        screen.blit(teleport_sprite1[10], teleport_sprite1[10].get_rect(center = (self.position)))
+        pygame.draw.circle(screen, "#3C8263", self.position, self.radius, LINE_WIDTH)
+        
     def split(self):
         self.kill()
-        for i in range(8):
+        for i in range(1, 8+1):
             angle = 40*i
             rotation = self.velocity.rotate(angle)
             shot = Shot(self.position.x, self.position.y)
